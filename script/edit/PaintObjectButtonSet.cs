b08813eaf6d3849f3676a145d8446d80a17de1b3
GreenSweet233 <139688734+GreenSweet233@users.noreply.github.com>
2026-07-02 21:33:28 +0800

    添加物品发光设置前端

--- script/edit/PaintObjectButtonSet.cs
+++ script/edit/PaintObjectButtonSet.cs
@@ -1,18 +1,36 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class PaintObjectButtonSet : Node {
 
     [Export] public EditManager.EditModeType PaintMode = EditManager.EditModeType.None;
     [Export] public PackedScene PaintObjectScene { get; set; } = null!;
 
+    [Export] public bool CanBeLighted {
+        get => _canBeLighted;
+        set {
+            _canBeLighted = value;
+            LightNode.Visible = value;
+        }
+    }
+    private bool _canBeLighted = false;
+    
+    [Export] public Control LightNode = null!;
+
     public EditManager? EditNode;
     private Button? _paintObjectButton;
 
     public override void _Ready() {
+        if (Engine.IsEditorHint()) {
+            LightNode.Visible = _canBeLighted;
+            return;
+        }
         _paintObjectButton = GetNode<Button>("..");
         _paintObjectButton.Pressed += OnPaintObjectButtonPressed;
         EditNode = GetTree().GetFirstNodeInGroup("edit_node") as EditManager;
+        
+        LightNode.Visible = _canBeLighted;
     }
 
     public void OnPaintObjectButtonPressed() {
